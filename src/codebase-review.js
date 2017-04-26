8d326bab7b07d85c5f0ae364348e1c4a92716556
Blake Turner <mail@blakewilliamturner.com>
2017-04-26 15:22:32 -0700

    Add change check, re-use empty branch

--- src/codebase-review.js
+++ src/codebase-review.js
@@ -1,38 +1,28 @@
 #! /usr/bin/env node
-const child_process = require('child_process');
 const argv = require('yargs').argv;
 require('colour');
+
+const runCommand = require('../lib/run-command');
+const { getRepoUrl, checkForUncommittedChanges } = require('../lib/git-helpers');
 
 const branchPrefix = argv.prefix || 'codebase-review';
 const emptyBranchName = `${branchPrefix}-empty`;
 const projectBranchName = `${branchPrefix}-project`;
 
-const execShellPromise = (command) => {
-  return new Promise((resolve, reject) => {
-    child_process.exec(command, function(error, stdout, stderr){
-      if (error) return reject(error);
-      resolve(stdout);
-    });
-  });
+const handleEmptyBranchCreation = () => {
+  return runCommand(`git branch -a | grep ${emptyBranchName}`)
+    .then(branch => {
+      if (!branch) return createAndPushEmptyBranch();
+      return runCommand(`git checkout ${emptyBranchName}`, `Using remote ${emptyBranchName}`);
+    })
 }
 
-const runCommand = (command, message) => {
-  if (message) console.log(message.green);
-  return execShellPromise(command);
-}
-
-const getRepoUrl = () => {
-  return runCommand('git config --get remote.origin.url').then(configUrl => {
-    // Remove .git extension
-    let url = configUrl.replace(/\s/, '').replace(/\.git$/, '') + '/compare';
-
-    // https clones
-    if (/^http/.test(url)) return url;
-
-    // ssh clones
-    return url.replace(/(^git.+:)?/, 'https://github.com/');
-  });
-}
+const createAndPushEmptyBranch = () => {
+  return runCommand(`git checkout --orphan ${emptyBranchName}`)
+    .then(() => runCommand('git rm -rf .', 'Clearing git house...'))
+    .then(() => runCommand(`git commit --allow-empty -m "Create ${emptyBranchName} branch"`, 'Commiting changes...'))
+    .then(() => runCommand(`git push --set-upstream origin ${emptyBranchName} --force`, `Pushing ${emptyBranchName} branch...`))
+};
 
 const run = () => {
   console.log('----------------------------------------------------------'.rainbow);
@@ -40,29 +30,36 @@ const run = () => {
   console.log();
 
   runCommand(`git branch -D ${emptyBranchName}`, 'Setting up workspace...')
+  .then(`git branch -D ${emptyBranchName}`, 'Setting up workspace...')
   .catch(() => true) // Swallow error if branch doesn't exist
   .then(() => runCommand(`git branch -D ${projectBranchName}`))
   .catch(() => true) // Swallow error if branch doesn't exist
-  .then(() => runCommand(`git checkout --orphan ${emptyBranchName}`))
-  .then(() => runCommand('git rm -rf .', 'Clearing git house...'))
-  .then(() => runCommand(`git commit --allow-empty -m "Create ${emptyBranchName} branch"`, 'Commiting changes...'))
-  .then(() => runCommand(`git push --set-upstream origin ${emptyBranchName} --force`, `Pushing ${emptyBranchName} branch...`))
+  .then(checkForUncommittedChanges)
+  .then(handleEmptyBranchCreation)
   .then(() => runCommand(`git checkout -b ${projectBranchName}`, `Creating new project branch(${projectBranchName})...`))
   .then(() => runCommand('git merge master --allow-unrelated-histories', 'Merging master into project branch...'))
   .then(() => runCommand(`git push --set-upstream origin ${projectBranchName} --force`, `Pushing ${projectBranchName} branch...`))
   .then(() => runCommand('git checkout master', 'Returning to master branch'))
+  .then(() => runCommand(`git branch -D ${emptyBranchName} ${projectBranchName}`, 'Clearing Codebase Review branches'))
   .then(getRepoUrl)
   .then((repoUrl) => {
     console.log();
     console.log('----------------------------------------------------------'.rainbow);
     console.log();
     console.log("You're done! Visit your git repo and make a new pull request:");
+    console.log();
     console.log(repoUrl);
     console.log();
     console.log('Base branch: ' + emptyBranchName.green);
     console.log('Compare branch: ' + projectBranchName.green);
     console.log();
   })
+  .catch(err => {
+    console.log();
+    console.log('Error:'.red);
+    console.log(err);
+    console.log();
+  });
 }
 
 run();
